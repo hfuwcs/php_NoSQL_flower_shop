d87734a77532168ff6920365cab62509e710563e
hfuwcs <luuphuc588@gmail.com>
2025-11-06 00:40:58 +0700

    feat: try to use mongodb atlas

--- config/database.php
+++ config/database.php
@@ -114,6 +114,7 @@ return [
         ],
         'mongodb' => [
             'driver' => 'mongodb',
+            'dns'=>env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             // 'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'homestead'),

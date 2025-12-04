0d6174e10eeb55d79f09fb057db8575fefbd6128
hfuwcs <luuphuc588@gmail.com>
2025-12-04 17:04:12 +0700

    #

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,8 +33,10 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 {{--Giỏ hàng--}}
-                <a href="{{ route('cart.index') }}" class="relative text-gray-500 hover:text-gray-700 mr-4">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">...</svg>
+                <a href="{{ route('cart.index') }}" class="relative text-gray-600 hover:text-gray-800 mr-4">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                    </svg>
                     @if(isset($cartItemCount) && $cartItemCount > 0)
                     <span class="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">{{ $cartItemCount }}</span>
                     @endif
